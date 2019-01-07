b8fd7d24362940975934d982eba06ff7b5e03fae
1618641 <1618641@qq.com>
2019-01-07 15:13:16 +0800

    1111111111111111

---
+++ src/main/java/com/example/DemoGit2Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGit2Application.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/aaa/One.java
@@ -0,0 +1,9 @@
+package com.example.aaa;
+
+/**
+ * @Author: 马果
+ * @Date: 2019/1/7 15:12
+ * @Description:
+ */
+public class One {
+}
